13675d2153d34c1845e4dbcef5c200cc3b1c9a6b
Foereaper <foe@elunatech.com>
2025-06-27 16:23:19 +0000

    Update Eluna documentation

---
+++ BigInt/sidebar.js
@@ -0,0 +1,15 @@
+document.write(`
+  <a id="BigInt:Add" class="fn" href="../BigInt/Add.html">Add</a>
+  <a id="BigInt:Divide" class="fn" href="../BigInt/Divide.html">Divide</a>
+  <a id="BigInt:Equal" class="fn" href="../BigInt/Equal.html">Equal</a>
+  <a id="BigInt:Equal" class="fn" href="../BigInt/Equal.html">Equal</a>
+  <a id="BigInt:Less" class="fn" href="../BigInt/Less.html">Less</a>
+  <a id="BigInt:LessOrEqual" class="fn" href="../BigInt/LessOrEqual.html">LessOrEqual</a>
+  <a id="BigInt:Mod" class="fn" href="../BigInt/Mod.html">Mod</a>
+  <a id="BigInt:Multiply" class="fn" href="../BigInt/Multiply.html">Multiply</a>
+  <a id="BigInt:Pow" class="fn" href="../BigInt/Pow.html">Pow</a>
+  <a id="BigInt:Subtract" class="fn" href="../BigInt/Subtract.html">Subtract</a>
+  <a id="BigInt:ToString" class="fn" href="../BigInt/ToString.html">ToString</a>
+  <a id="BigInt:ToString" class="fn" href="../BigInt/ToString.html">ToString</a>
+  <a id="BigInt:UnaryMinus" class="fn" href="../BigInt/UnaryMinus.html">UnaryMinus</a>
+`);
